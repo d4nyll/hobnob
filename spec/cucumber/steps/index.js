280ea292e53d267f636f0b57526a8b828808e7a9
Daniel Li <dan@danyll.com>
2018-09-03 00:38:12 +0100

    Refactor test code

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import superagent from 'superagent';
 import { When, Then } from 'cucumber';
+import { getValidPayload, convertStringToArray } from './utils';
 
 When(/^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/, function (method, path) {
   this.request = superagent(method, `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}${path}`);
@@ -63,22 +64,16 @@ When(/^without a (?:"|')([\w-]+)(?:"|') header set$/, function (headerName) {
 });
 
 When(/^attaches an? (.+) payload which is missing the ([a-zA-Z0-9, ]+) fields?$/, function (payloadType, missingFields) {
-  const payload = {
-    email: 'e@ma.il',
-    password: 'password',
-  };
-  const fieldsToDelete = missingFields.split(',').map(s => s.trim()).filter(s => s !== '');
-  fieldsToDelete.forEach(field => delete payload[field]);
+  this.requestPayload = getValidPayload(payloadType);
+  const fieldsToDelete = convertStringToArray(missingFields);
+  fieldsToDelete.forEach(field => delete this.requestPayload[field]);
   this.request
-    .send(JSON.stringify(payload))
+    .send(JSON.stringify(this.requestPayload))
     .set('Content-Type', 'application/json');
 });
 
 When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are)(\s+not)? a ([a-zA-Z]+)$/, function (payloadType, fields, invert, type) {
-  const payload = {
-    email: 'e@ma.il',
-    password: 'password',
-  };
+  this.requestPayload = getValidPayload(payloadType);
   const typeKey = type.toLowerCase();
   const invertKey = invert ? 'not' : 'is';
   const sampleValues = {
@@ -87,25 +82,22 @@ When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are)(\
       not: 10,
     },
   };
-  const fieldsToModify = fields.split(',').map(s => s.trim()).filter(s => s !== '');
+  const fieldsToModify = convertStringToArray(fields);
   fieldsToModify.forEach((field) => {
-    payload[field] = sampleValues[typeKey][invertKey];
+    this.requestPayload[field] = sampleValues[typeKey][invertKey];
   });
   this.request
-    .send(JSON.stringify(payload))
+    .send(JSON.stringify(this.requestPayload))
     .set('Content-Type', 'application/json');
 });
 
 When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are) exactly (.+)$/, function (payloadType, fields, value) {
-  const payload = {
-    email: 'e@ma.il',
-    password: 'password',
-  };
-  const fieldsToModify = fields.split(',').map(s => s.trim()).filter(s => s !== '');
+  this.requestPayload = getValidPayload(payloadType);
+  const fieldsToModify = convertStringToArray(fields);
   fieldsToModify.forEach((field) => {
-    payload[field] = value;
+    this.requestPayload[field] = value;
   });
   this.request
-    .send(JSON.stringify(payload))
+    .send(JSON.stringify(this.requestPayload))
     .set('Content-Type', 'application/json');
 });
