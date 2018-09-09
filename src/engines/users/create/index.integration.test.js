7f2a2ababc5fb6ec7b708109dc2ae0704c787bb3
Daniel Li <dan@danyll.com>
2018-09-09 12:44:19 +0100

    Add integration tests for Create User engine

---
+++ src/engines/users/create/index.integration.test.js
@@ -0,0 +1,35 @@
+import assert from 'assert';
+import elasticsearch from 'elasticsearch';
+import ValidationError from '../../../validators/errors/validation-error';
+import createUserValidator from '../../../validators/users/create';
+import create from '.';
+
+const db = new elasticsearch.Client({
+  host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
+});
+
+describe('User Create Engine', function () {
+  describe('When invoked with invalid req', function () {
+    it('should return promise that rejects with an instance of ValidationError', function () {
+      const req = {};
+      create(req, db, createUserValidator, ValidationError)
+        .catch(err => assert(err instanceof ValidationError));
+    });
+  });
+  describe('When invoked with valid req', function () {
+    it('should return a success object containing the user ID', function () {
+      const req = {
+        body: {
+          email: 'e@ma.il',
+          password: 'password',
+          profile: {},
+        },
+      };
+      create(req, db, createUserValidator, ValidationError)
+        .then((result) => {
+          assert.equal(result.result, 'created');
+          assert.equal(typeof result._id, 'string');
+        });
+    });
+  });
+});
