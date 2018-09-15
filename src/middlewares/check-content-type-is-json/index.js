bfda438703442f79cd311eac256c2777a5b5909a
Daniel Li <dan@danyll.com>
2018-09-15 12:44:07 +0100

    Increase specificity of checkContentTypeIsJson

--- src/middlewares/check-content-type-is-json/index.js
+++ src/middlewares/check-content-type-is-json/index.js
@@ -1,5 +1,5 @@
 function checkContentTypeIsJson(req, res, next) {
-  if (!req.headers['content-type'].includes('application/json')) {
+  if (req.headers['content-type'] && !req.headers['content-type'].includes('application/json')) {
     res.status(415);
     res.set('Content-Type', 'application/json');
     return res.json({ message: 'The "Content-Type" header must always be "application/json"' });
