f82823fe1c8c75a56e2893c78923d3bf8417e878
Daniel Li <dan@danyll.com>
2018-09-22 21:15:51 +0100

    Return immediately after res.send or res.json

--- src/handlers/auth/login/index.js
+++ src/handlers/auth/login/index.js
@@ -3,21 +3,20 @@ function login(req, res, db, engine, validator, ValidationError, sign) {
     .then((result) => {
       res.status(200);
       res.set('Content-Type', 'text/plain');
-      res.send(result);
+      return res.send(result);
     })
     .catch((err) => {
       res.set('Content-Type', 'application/json');
       if (err instanceof ValidationError) {
         res.status(400);
-        res.json({ message: err.message });
-      } else if (err.message === 'Not Found') {
-        res.status(401);
-        res.json({ message: 'There are no records of an user with this email and password combination' });
-      } else {
-        res.status(500);
-        res.json({ message: 'Internal Server Error' });
+        return res.json({ message: err.message });
       }
-      return err;
+      if (err.message === 'Not Found') {
+        res.status(401);
+        return res.json({ message: 'There are no records of an user with this email and password combination' });
+      }
+      res.status(500);
+      return res.json({ message: 'Internal Server Error' });
     });
 }
 
